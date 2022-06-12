73a05667db26f4e0cd751b8a6fb390dc0997526c
Ivan Moctezuma <ivan_0617_@hotmail.com>
2022-06-11 23:58:33 -0500

    proyecto actualizado hasta la clase de instalacion de mapStruct

---
+++ src/main/java/com/example/market/persistence/entity/Categoria.java
@@ -0,0 +1,55 @@
+package com.example.market.persistence.entity;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    private String descripcion;
+
+    private Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> prouctos;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public List<Producto> getProuctos() {
+        return prouctos;
+    }
+
+    public void setProuctos(List<Producto> prouctos) {
+        this.prouctos = prouctos;
+    }
+}
---
+++ src/main/java/com/example/market/persistence/entity/ComprasProducto.java
@@ -0,0 +1,71 @@
+package com.example.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "compras_producto")
+public class ComprasProducto {
+
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compre;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
+
+    public ComprasProductoPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Compra getCompre() {
+        return compre;
+    }
+
+    public void setCompre(Compra compre) {
+        this.compre = compre;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
